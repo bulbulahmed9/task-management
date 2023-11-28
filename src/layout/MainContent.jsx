dbd8f3cbac4d5adf6574ca5e76021c26b62d3ed5
Md. Bulbul Ahmed <bulbulahmed.tk@gmail.com>
2023-11-29 00:37:08 +0600

    feat : re-structure folder

--- src/layout/MainContent.jsx
+++ src/layout/MainContent.jsx
@@ -1,13 +1,8 @@
-import { Button } from "antd";
 import TaskLanding from "../Modules/task/TaskLanding";
 
 const MainContent = () => {
   return (
     <div className="container">
-      <div className="container-header d-flex justify-content-between align-items-center">
-        <h3>Task Landing</h3>
-        <Button type="primary">Create Task</Button>
-      </div>
       <TaskLanding />
     </div>
   );
--- src/modules/task/TaskLanding.jsx
+++ src/modules/task/TaskLanding.jsx
@@ -1,9 +1,13 @@
-import { Table } from "antd";
-import { columns, sampleData } from "/utils";
+import { Button, Table } from "antd";
+import { columns, sampleData } from "./utils";
 
 const TaskLanding = () => {
   return (
     <div>
+      <div className="landing-header d-flex justify-content-between align-items-center">
+        <h3>Task Landing</h3>
+        <Button type="primary">Create Task</Button>
+      </div>
       <Table
         scroll={{
           x: 800,
